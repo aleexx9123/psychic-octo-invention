9b2e056e842972875a5f8bb68097f7cb523746e0
aleexx9123 <raulzz0u789@gmail.com>
2026-07-27 03:47:57 +0200

    Point loader to customized YARHM 1.21

--- yarhm.lua
+++ yarhm.lua
@@ -1,17 +1,19 @@
 local src = ""
 local CoreGui = game:GetService("StarterGui")
 
-pcall(function() 
-    src = game:HttpGet("https://rawscripts.net/raw/Universal-Script-YARHM-12403", false)
+pcall(function()
+	src = game:HttpGet(
+		"https://raw.githubusercontent.com/aleexx9123/psychic-octo-invention/main/source/yarhm/1.21/yarhm.lua",
+		false
+	)
 end)
 if src == "" then
-  CoreGui:SetCore("SendNotification", {
-  	Title = "YARHM Outage";
-  	Text = "Scriptblox is currently unavailable! Sorry for the inconvenience. Using YARHM Offline.";
-	  Duration = 5;
-  })
-  src = game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.20/yarhm.lua", false)
+	CoreGui:SetCore("SendNotification", {
+		Title = "YARHM Outage",
+		Text = "The latest YARHM build is currently unavailable. Using the mirror.",
+		Duration = 5,
+	})
+	src = game:HttpGet("https://rawscripts.net/raw/Universal-Script-YARHM-12403", false)
 end
-
 
 loadstring(src)()
